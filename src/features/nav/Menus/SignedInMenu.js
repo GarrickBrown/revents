66ff12722c33b5bc98749e493d8501783cfea205
Garrick Brown <41980127+GarrickBrown@users.noreply.github.com>
2019-01-25 19:12:20 +0800

    Final changes

--- src/features/nav/Menus/SignedInMenu.js
+++ src/features/nav/Menus/SignedInMenu.js
@@ -8,9 +8,8 @@ const SignedInMenu = ({ logout, profile, auth }) => {
 			<Image avatar spaced="right" src={profile.photoURL || '/assets/user.png'} />
 			<Dropdown pointing="top left" text={profile.displayName}>
 				<Dropdown.Menu>
-					<Dropdown.Item text="Create Event" icon="plus" />
-					<Dropdown.Item text="My Events" icon="calendar" />
-					<Dropdown.Item text="My Network" icon="users" />
+					<Dropdown.Item as={Link} to={'/createEvent'} text="Create Event" icon="plus" />
+					<Dropdown.Item as={Link} to={'/people'} text="My Network" icon="users" />
 					<Dropdown.Item as={Link} to={`/profile/${auth.uid}`} text="My Profile" icon="user" />
 					<Dropdown.Item as={Link} to="/settings" text="Settings" icon="settings" />
 					<Dropdown.Item text="Logout" icon="power" onClick={logout} />
